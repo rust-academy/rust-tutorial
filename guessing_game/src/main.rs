f9dd1737b2f7d96d8a4263aaacbde1e07442c5f1
Marvin Hansen <marvin.hansen@gmail.com>
2022-11-15 10:21:08 +0800

    Added code for second chapter, guessing game.

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+use std::cmp::Ordering;
+use colored::*;
+
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1 ..=10);
+
+    loop{
+        println!("Guess the number");
+        let mut read_guess = String::new();
+
+        io::stdin()
+            .read_line(&mut read_guess)
+            .expect("Error reading from command line");
+
+        let guess: u32 = match read_guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {println!("{}", "Too small!".red())},
+            Ordering::Greater => {println!("{}", "Too big!".red())}
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
+}
