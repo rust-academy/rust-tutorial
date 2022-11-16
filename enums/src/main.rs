81d5e499e2baa1d085a6ef71ce7389a9e7cdefa8
Marvin Hansen <marvin.hansen@gmail.com>
2022-11-16 14:29:03 +0800

    Added code for sixth chapter, enums.

---
+++ enums/src/main.rs
@@ -0,0 +1,25 @@
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
+
+mod ip_addr;
+mod optional;
+
+fn main() {
+    println!("Hello, enum!");
+
+    test_ip_enum();
+    optional::test_optional_enum();
+
+}
+
+fn test_ip_enum(){
+    let ip_four_localhost = Ipv4Addr::new(127, 0, 0, 1);
+    let ip_six_localhost = Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1);
+
+    let home = ip_addr::IpAddr::V4(ip_four_localhost);
+    let home_v6 = ip_addr::IpAddr::V6(ip_six_localhost);
+
+    println!("{:?}", home);
+    println!("{:?}", home_v6);
+}
+
