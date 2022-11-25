988eb9520f197d7253bcba202f34b7945768bc1b
Marvin Hansen <marvin.hansen@gmail.com>
2022-11-25 20:15:47 +0800

    Added code for chapter nine: error handling.

---
+++ errors/src/main.rs
@@ -0,0 +1,30 @@
+mod errors;
+
+const UNRECOVERABLE: bool = false;
+
+
+fn main() {
+    if UNRECOVERABLE {
+        unrecoverable();
+    } else{
+        recoverable();
+    }
+}
+
+fn unrecoverable(){
+    // test_panic();
+    // errors::open_file();
+
+    let path = "Hello.txt";
+    errors::result_match_panic(path);
+}
+
+fn test_panic() {
+    panic!("test panic");
+}
+
+
+fn recoverable(){
+    let path = "Hello.txt";
+    errors::recoverable_file_open(path);
+}
