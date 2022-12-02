75cd46b666f9c00d79185a2870e286cd1dd276f8
Marvin Hansen <marvin.hansen@gmail.com>
2022-12-02 19:05:37 +0800

    Added simple cli app for chapter 12.

---
+++ cli/src/main.rs
@@ -0,0 +1,13 @@
+mod simple;
+
+use std::{env, fs};
+
+// Requirements
+// 1) Parse two command line argument, the word to search and the file path
+// 2) Read the file from the path into a string
+// 3) Search the word in text from the file
+// 4) Print out each line where the word occurs
+fn main() {
+     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args)
+}
