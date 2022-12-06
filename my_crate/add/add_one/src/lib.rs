a771b39c691e7de28b33aa76029f8352c2fc3449
Marvin Hansen <marvin.hansen@gmail.com>
2022-12-06 19:20:20 +0800

    Added code for chapter 14, cargo

---
+++ my_crate/add/add_one/src/lib.rs
@@ -0,0 +1,15 @@
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_one() {
+        let result = add_one(3);
+        assert_eq!(result, 4);
+    }
+}
---
+++ my_crate/art/src/main.rs
@@ -0,0 +1,11 @@
+use art::PrimaryColor;
+use art::utils::mixl;
+
+fn main() {
+    println!("Hello, world!");
+
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+
+    mix(red, yellow);
+}
