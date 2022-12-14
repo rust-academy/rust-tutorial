0a422641038e64437e875149247b7b991154639d
Marvin Hansen <marvin.hansen@gmail.com>
2022-12-14 20:45:26 +0800

    Added and updated code for chapter 15, smart pointers.

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,15 +1,34 @@
 mod box_type;
 mod my_box;
 mod csp;
+mod rc_type;
 
 fn main() {
+    println!("==test_box==");
     box_type::test_box();
-    box_type::test_cons_list();
-    my_box::test_deref();
-    my_box::test_deref_coercion();
-    my_box::test_deref_my_box();
+    println!();
 
+    println!("==test_cons_list==");
+    box_type::test_cons_list();
     println!();
+
+    println!("==test_deref==");
+    my_box::test_deref();
     println!();
+
+    println!("==test_deref_coercion==");
+    my_box::test_deref_coercion();
+    println!();
+
+    println!("==test_deref_my_box==");
+    my_box::test_deref_my_box();
+    println!();
+
+    println!("==test_custom_smart_pointer==");
     csp::test_custom_smart_pointer();
+    println!();
+
+    println!("==test_ref_count==");
+    rc_type::test_ref_count();
+    println!();
 }
---
+++ smart_pointers/src/rc_type.rs
@@ -0,0 +1,45 @@
+use std::rc::Rc;
+
+enum ConsList{
+    Cons(i32, Box<ConsList>),
+    Nil,
+}
+
+// Doesn't work b/c Box cannot handle multiple owners
+pub fn _test_mut_list_owners(){
+    // let a = ConsList::Cons(5, Box::new(ConsList::Cons(10, Box::new(ConsList::Nil))));
+    // let _b = ConsList::Cons(3, Box::new(a)); //  value a moved here
+    // let c = ConsList::Cons(3, Box::new(a)); // Error: ^ value used here after move
+}
+
+#[derive(Debug)]
+enum List{
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::rc_type::List::{Cons, Nil};
+
+pub fn test_ref_count() {
+    // You have to enable multiple ownership explicitly by using the Rust type Rc<T>,
+    // which is an abbreviation for reference counting.
+
+    // The Rc<T> type keeps track of the number of references to a value to determine
+    // whether or not the value is still in use.
+
+    // If there are zero references to a value,
+    // the value can be cleaned up without any references becoming invalid. (meaning memory will be cleaned up)
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("Ref Count after creating a = {}", Rc::strong_count(&a));
+
+    let _b = Cons(3, Rc::clone(&a));
+    println!("Ref Count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let _c = Rc::new(Cons(4, Rc::clone(&a)));
+        println!("Ref count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("Ref count after c goes out of scope = {}", Rc::strong_count(&a));
+
+} // b & a go out of scope so its memory will be cleaned
